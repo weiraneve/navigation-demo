1d5d84b506e19ab90f2a5cc18a520b7196da3679
weiran1999 <907221539@qq.com>
2023-02-16 09:31:09 +0800

    init

---
+++ app/src/main/java/com/weiran/navigation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.weiran.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
