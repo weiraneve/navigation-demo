34045ada234f22e2c9edc230e17c92fba4cd2aeb
weiran1999 <907221539@qq.com>
2023-03-07 16:46:56 +0800

    refactor: update lazyColumn style

--- app/src/main/java/com/weiran/navigation/ui/compose/page/ForYouScreen.kt
+++ app/src/main/java/com/weiran/navigation/ui/compose/page/ForYouScreen.kt
@@ -1,19 +1,23 @@
 package com.weiran.navigation.ui.compose.page
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ForYouScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
         Text("ForYou")
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            (0..100).forEach {
-                item { 
+            (0..20).forEach {
+                item(it) {
+                    Spacer(modifier = Modifier.height(100.dp))
                     Text(text = it.toString())
                 }
             }
